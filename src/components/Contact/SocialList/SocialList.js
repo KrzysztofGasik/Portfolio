497b65e23b15bff3187747f67b2f5053b21dbccc
KrzysztofGasik <kgasik@gmail.com>
2022-03-20 12:22:34 +0100

    changed url to GH in social list

--- src/components/Contact/SocialList/SocialList.js
+++ src/components/Contact/SocialList/SocialList.js
@@ -7,17 +7,34 @@ import classes from "./SocialList.module.css";
 import SocialElement from "../SocialElement/SocialElement";
 
 const socialListArray = [
-  { href: "https://github.com/KrzGas", icon: faGithub, class: classes.GHIcon },
-  { href: "https://www.linkedin.com/in/krzysztof-gasik", icon: faLinkedin, class: classes.LinkedinIcon },
-  { href: "mailto:kgasik@gmail.com", icon: faEnvelope, class: classes.MailIcon }
+  {
+    href: "https://github.com/KrzysztofGasik",
+    icon: faGithub,
+    class: classes.GHIcon,
+  },
+  {
+    href: "https://www.linkedin.com/in/krzysztof-gasik",
+    icon: faLinkedin,
+    class: classes.LinkedinIcon,
+  },
+  {
+    href: "mailto:kgasik@gmail.com",
+    icon: faEnvelope,
+    class: classes.MailIcon,
+  },
 ];
 
 const SocialList = () => {
   return (
     <div className={classes.SocialListWrapper}>
       <ul className={classes.SocialList}>
-        {socialListArray.map(el => (
-          <SocialElement key={el.href} href={el.href} icon={el.icon} iconClass={el.class}/>
+        {socialListArray.map((el) => (
+          <SocialElement
+            key={el.href}
+            href={el.href}
+            icon={el.icon}
+            iconClass={el.class}
+          />
         ))}
       </ul>
     </div>
